f8d3a59c059de13ec1d916b485b7afafe44e0a2e
Jefferson on ist320 vbox <jefferson.eagley@gmail.com>
2016-09-28 16:26:37 -0700

    ObjectBasicsJasmine added, not complete

---
+++ Week02/Week02-ObjectBasicsJasmine/Source/MyObject.js
@@ -0,0 +1,19 @@
+var myObject = {
+    herp: 1,
+    derp: 2,
+    gonk: 'gonk',
+    fun1: function () {
+        'use strict';
+        return 1;
+    },
+    fun2: function () {
+        'use strict';
+        return 2;
+    },
+    fun3: function () {
+        'use strict';
+        return 3;
+    }
+};
+
+exports.myObject;
