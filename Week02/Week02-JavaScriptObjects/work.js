afe04ae6dc1f935f931e6011fdaba662d9a3cbdb
Jefferson on ist320 vbox <jefferson.eagley@gmail.com>
2016-09-29 16:23:46 -0700

    grunt check on jsobjects

--- Week02/Week02-JavaScriptObjects/work.js
+++ Week02/Week02-JavaScriptObjects/work.js
@@ -1,45 +1,50 @@
 var person = {
-    firstName: "George",
-    lastName: "Washington",
-    fullName: function () {
-        return this.firstName + " " + this.lastName;
+    firstName: 'Jefferson',
+    lastName: 'Eagley',
+    fullName: function() {
+        'use strict';
+        return this.firstName + ' ' + this.lastName;
     }
-}
+};
 
 var calculator = {
     operator1: -1,
     operator2: -1,
-    add: function () {
+    add: function() {
+        'use strict';
         return this.operator2 + this.operator1;
     },
-    subtract: function () {
+    subtract: function() {
+        'use strict';
         return this.operator1 - this.operator2;
     }
-}
+};
 
-calculator.multiply = function () {
+calculator.multiply = function() {
+    'use strict';
     return calculator.operator1 * calculator.operator2;
-}
+};
 
 function divider(title) {
-    console.log("====================================");
+    'use strict';
+    console.log('====================================');
     console.log(title);
-    console.log("====================================");
+    console.log('====================================');
 }
 
-divider("George Washington");
+divider('Person');
 
 console.log(person.firstName);
 console.log(person.lastName);
 console.log(person.fullName());
 
-divider("Calculator");
+divider('Calculator');
 
 calculator.operator1 = person.firstName.length;
 calculator.operator2 = person.lastName.length;
 
-console.log("Operator01 = " + calculator.operator1);
-console.log("Operator02 = " + calculator.operator2);
-console.log("add = " + calculator.add());
-console.log("sub = " + calculator.subtract());
-console.log("mult = " + calculator.multiply());
+console.log('Operator01 = ' + calculator.operator1);
+console.log('Operator02 = ' + calculator.operator2);
+console.log('add = ' + calculator.add());
+console.log('sub = ' + calculator.subtract());
+console.log('mult = ' + calculator.multiply());
