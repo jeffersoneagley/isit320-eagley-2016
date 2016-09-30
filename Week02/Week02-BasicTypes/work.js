5b02d407c84c6236db73503edd55b2d840518f52
Jefferson on ist320 vbox <jefferson.eagley@gmail.com>
2016-09-29 17:43:56 -0700

    set up BasicTypes and gave [broken]BasicTypesExpress it's own folder

---
+++ Week02/Week02-BasicTypes/work.js
@@ -0,0 +1,17 @@
+const fs = require('fs');
+
+fs.readFile('myJSONTestData.json', 'utf8', (err, rawData) => {
+    if (err) {
+        console.log(err);
+        throw err;
+    }
+    var myData = JSON.parse(rawData);
+    console.log(myData);
+    console.log(typeof myData);
+    console.log("Content:");
+    for (var entry in myData) {
+        if (myData.hasOwnProperty(entry)) {
+            console.log(typeof myData[entry] + "\t" + entry + ":\t" + myData[entry]);
+        }
+    }
+})
