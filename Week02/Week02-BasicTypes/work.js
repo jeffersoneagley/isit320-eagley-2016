9ce6027ca6604157f0d8e131b689078cc4150c5f
Jefferson on ist320 vbox <jefferson.eagley@gmail.com>
2016-10-08 09:53:34 -0700

    fixed grunt on basictypes

--- Week02/Week02-BasicTypes/work.js
+++ Week02/Week02-BasicTypes/work.js
@@ -1,6 +1,7 @@
-const fs = require('fs');
+var fs = require('fs');
 
-fs.readFile('myJSONTestData.json', 'utf8', (err, rawData) => {
+fs.readFile('myJSONTestData.json', 'utf8', function(err, rawData) {
+    'use strict';
     if (err) {
         console.log(err);
         throw err;
@@ -8,10 +9,10 @@ fs.readFile('myJSONTestData.json', 'utf8', (err, rawData) => {
     var myData = JSON.parse(rawData);
     console.log(myData);
     console.log(typeof myData);
-    console.log("Content:");
+    console.log('Content:');
     for (var entry in myData) {
         if (myData.hasOwnProperty(entry)) {
-            console.log(typeof myData[entry] + "\t" + entry + ":\t" + myData[entry]);
+            console.log(typeof myData[entry] + '\t' + entry + ':\t' + myData[entry]);
         }
     }
-})
+});
