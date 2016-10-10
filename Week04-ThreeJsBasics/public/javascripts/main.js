6e32450dbd9fd4f2f850ec462e4cafc169d156a3
Jefferson on ist320 vbox <jefferson.eagley@gmail.com>
2016-10-10 16:18:12 -0700

    ThreeJSgit add .!

---
+++ Week04-ThreeJsBasics/public/javascripts/main.js
@@ -0,0 +1,25 @@
+requirejs.config({
+    baseUrl: '.',
+    paths: {
+        'jquery': 'components/jquery/dist/jquery',
+        'bootstrap': 'components/bootstrap/dist/js/bootstrap',
+        'Three': 'javascripts/three.min',
+        'control': 'javascripts/control'
+
+    },
+    shim: {
+        'Three': {
+            exports: 'THREE'
+        }
+    }
+});
+
+requirejs(['jquery'], function($) {
+    'use strict';
+    requirejs(['bootstrap', 'Three', 'control'], function(bootstrap, THREE, Control) {
+        $(document)
+            .ready(function() {
+                var control = new Control(THREE);
+            });
+    });
+});
