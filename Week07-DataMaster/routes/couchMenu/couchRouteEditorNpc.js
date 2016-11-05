31eae994019a0eba5e01ffb93ba82c0c0dd80bef
Jefferson on ist320 vbox <jefferson.eagley@gmail.com>
2016-11-04 22:16:14 -0700

    working on my couch menu

--- Week07-DataMaster/routes/couchMenu/couchRouteEditorNpc.js
+++ Week07-DataMaster/routes/couchMenu/couchRouteEditorNpc.js
@@ -1,26 +1,62 @@
 function EditorNpc(router) {
     'use strict';
 
-    function BuildNpcListHtml() {
-        return 'derp';
+    var req = null;
+
+    function BuildNpcListHtml(callback) {
+        BuildNpcListElement({
+            'name': 'derp',
+            'npc_id': 1
+        }, function(htmlText) {
+            callback(htmlText);
+        });
     }
 
-    function BuildNpcListElement() {
-        return app.render('template/npcLine.jade', {
-            'npc_name': 'myName',
-            'npc_id': 1
-        }, function functionName() {
+    function BuildNpcListElement(npc, callback) {
+        req.app.render('template/npcLine.jade', {
+            'npc_name': npc.name,
+            'npc_id': npc.npc_id
+        }, function(err, result) {
+            console.log(result);
+            if (!err) {
+                callback(result);
+            } else {
+                console.log(err);
+                throw err;
+            }
+        });
+    }
 
+    function onNpcListRecieved(dataFromDb) {
+
+        BuildNpcListHtml(function(htmlSnippet) {
+            var myEditorInterface = {
+                'title': 'NPC Database',
+                'body': htmlSnippet
+            };
+            console.log(myEditorInterface);
+            response.send(myEditorInterface);
         });
     }
 
     router.get('/editor/npc', function(request, response) {
-        var myEditorInterface = {
-            'title': 'NPC Database',
-            'body': BuildNpcListHtml()
-        };
-        console.log(myEditorInterface);
-        response.send(myEditorInterface);
+        try {
+            console.log('editor/npc');
+            req = request;
+            req.app.get('/docNpcAllByID', function(thing1, thing2) {
+                console.log('polling against db');
+                console.log(thing1);
+                console.log(thing2);
+            });
+        } catch (e) {
+            console.log(e);
+            response.send({
+                'title': 'err',
+                'body': JSON.stringify(e)
+            });
+        } finally {
+
+        }
     });
 }
 module.exports = EditorNpc;
