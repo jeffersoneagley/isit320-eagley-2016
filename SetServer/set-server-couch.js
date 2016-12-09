18934fcdf5aa1651640d8c2cc88f3ea2bf90ec07
Jefferson on ist320 vbox <jefferson.eagley@gmail.com>
2016-12-08 19:19:09 -0800

    setserver default to 0

--- SetServer/set-server-couch.js
+++ SetServer/set-server-couch.js
@@ -4,7 +4,7 @@ var servers = ['http://127.0.0.1:5984',
     'http://168.156.41.96:5984',
     'http://10.0.2.5:5984'
 ];
-var serverIndex = 4;
+var serverIndex = 0;
 var serverUrl = servers[serverIndex];
 console.log('SetServer middleware attaching to database on: ', serverUrl);
 
